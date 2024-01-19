6e00666ca6bec047fbcee126db8d4afd6d76e4f5
Jay Vaghani <jayvaghani2000@gmail.com>
2024-01-19 14:00:43 +0530

    upload projects

--- src/data/work.ts
+++ src/data/work.ts
@@ -1,64 +1,40 @@
-import METAVERSE from "@/assets/posters/metaverse.png";
-import TIXID from "@/assets/posters/tixId.png";
-import MINTY from "@/assets/posters/minty.png";
-import AIRBNB from "@/assets/posters/airbnb.png";
-import MOVIEBOX from "@/assets/posters/movieBox.png";
+import GLEANS from "@/assets/posters/gleans.png";
+import NILO from "@/assets/posters/nilo.png";
+import CANDIDATE_BAZAAR from "@/assets/posters/candidateBazaar.png";
+import CLOUD_EAGLE from "@/assets/posters/cloudeagle.png";
+
 const Data = [
   {
     _id: 1,
-    name: "Tix ID",
-    link: "https://tix-id.vercel.app/login",
+    name: "Gleans",
+    link: "https://gleans.me",
     date: "2022-05-24T11:23:38.543Z",
-    tags: ["React", "Nextjs", "Redux", "NextAuth", "MUI"],
-    poster: TIXID,
+    tags: ["Next Js", "Supabase", "Prisma", "Typescript", "Framer Motion"],
+    poster: GLEANS,
   },
   {
     _id: 2,
-    name: "Minty",
-    link: "http://minty-peach.vercel.app/",
-    date: "2022-05-18T11:23:38.543Z",
-    tags: ["Javascript Vanilla", "SCSS", "Parcel"],
-    poster: MINTY,
+    name: "Nilo",
+    link: "https://www.nilo.gg/",
+    date: "2022-05-24T11:23:38.543Z",
+    tags: ["React Js", "Typescript", "Node Js", "Nest Js", "MUI", "Redux"],
+    poster: NILO,
   },
-
   {
     _id: 3,
-    name: "MetaVerse",
-    link: "https://meta-verse-gamma.vercel.app/",
-    date: "2023-10-07T11:23:38.543Z",
-    tags: ["React", "Nextjs", "FRAMER MOTION"],
-    poster: METAVERSE,
+    name: "Candidate Bazaar",
+    link: "https://candidates-bazaar.surge.sh/login",
+    date: "2022-05-24T11:23:38.543Z",
+    tags: ["React Js", "Typescript", "Antd", "React Table", "React PDF"],
+    poster: CANDIDATE_BAZAAR,
   },
-
   {
     _id: 4,
-    name: "Airbnb Clone",
-    link: "https://airbnb-clone-site-mu.vercel.app/",
-    date: "2023-10-16T11:23:38.543Z",
-    tags: [
-      "Nextjs-13",
-      "MongoDB",
-      "NextAuth",
-      "Zustand",
-      "Tailwind",
-      "Typescript",
-    ],
-    poster: AIRBNB,
-  },
-  {
-    _id: 5,
-    name: "MovieBox",
-    link: "https://movieboxes.vercel.app/movie/753342",
-    date: "2023-10-16T11:23:38.543Z",
-    tags: [
-      "Nextjs-13",
-      "TMDB-api",
-      "Zustand",
-      "Tailwind",
-      "Typescript",
-      "Clerk Auth",
-    ],
-    poster: MOVIEBOX,
+    name: "Cloudeagle",
+    link: "https://somecorp.cloudeagle.us/app/signin",
+    date: "2022-05-24T11:23:38.543Z",
+    tags: ["React Js", "Zustand", "React Mention", "AntX6"],
+    poster: CLOUD_EAGLE,
   },
 ];
 
